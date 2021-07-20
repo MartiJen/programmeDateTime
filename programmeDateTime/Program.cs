ed36d22b5fefafac80b25f1f0e802a1a12add6ac
[Martijen] <jennifer_martin@outlook.fr>
2021-07-20 17:29:31 +0200

    Ajoutez des fichiers projet.

---
+++ programmeDateTime/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace programmeDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = DateTime.Now;
+
+            //Console.WriteLine(date);
+
+            CultureInfo cultureFrançais = CultureInfo.GetCultureInfo("fr-FR");
+
+            Console.WriteLine(date.ToString("dddd dd MMMM yyyy HH:mm:ss", cultureFrançais));
+
+            DateTime dateDemain = date.AddDays(1);
+
+            Console.WriteLine("Demain : " + dateDemain.ToString("dddd dd MMMM yyyy HH:mm:ss", cultureFrançais));
+
+            var diff = dateDemain - date;
+            Console.WriteLine("Différence heures : " + diff.TotalHours);
+
+        }
+    }
+}
